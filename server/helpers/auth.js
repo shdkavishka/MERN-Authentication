8249bd6407c40f6a714af7c457f5202aedd8cc74
Deshan Kavishka <99667129+shdkavishka@users.noreply.github.com>
2024-03-11 01:02:34 +0530

    Login Successfull

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,35 +1,37 @@
 const bcrypt = require('bcrypt');
 
-// const hashPassword = (password) => {
-//     return new promise((resolve, reject) => {
-//         bcrypt.genSalt(12 , (err,salt)=>{
-//             if (err) return reject(err);
-//             bcrypt.hash(password,salt, (err,hash)=>{
-//                 if (err) return reject(err);
-//                 resolve(hash);
-//             })
-//         })
-//     })
-// }
+const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(12 , (err,salt)=>{
+            if (err) return reject(err);
+            bcrypt.hash(password,salt, (err,hash)=>{
+                if (err) return reject(err);
+                resolve(hash);
+            })
+        })
+    })
+}
 
+const comparePassword = (password, hashed) => {
+    return bcrypt.compare(password, hashed);
+}
+
+
+// const hashPassword = async (password) => {
+//     try {
+//         const hash =await bcrypt.hash(password,12);
+//         return hash;
+//     }
+//     catch{
+//         console.log(error)
+//     }
+// }
 // const comparePassword = (password, hashed) => {
 //     return bcrypt.compare(password, hashed);
 // }
 
 
-const hashPassword = async (password) => {
-    try {
-        const hash =await bcrypt.hash(password,12);
-        return hash;
-    }
-    catch{
-        console.log(error)
-    }
-}
-
-
-
 module.exports = {
     hashPassword,
-    // comparePassword
+    comparePassword
 }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const {test,registerUser} = require('../controllers/authController');
+const {test,registerUser,loginUser} = require('../controllers/authController');
 
 //middleware
 router.use(cors({
@@ -11,5 +11,6 @@ router.use(cors({
 
 router.get('/',test)
 router.post('/register' , registerUser)
+router.post ('/login' , loginUser)
 
 module.exports = router;
